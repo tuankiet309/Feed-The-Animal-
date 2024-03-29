b2d561f9a3c3663d8623d8410b0350cfbc1b32bc
Tuan Kiet <nguyentuankiet309203@gmail.com>
2024-03-29 19:59:08 +0700

    Finish the prototype

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float spawnTimer = 0.5f;
+    public float spawnTimerCounter = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimerCounter -= Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space)&&spawnTimerCounter<0)
+        {
+            spawnTimerCounter = spawnTimer;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
